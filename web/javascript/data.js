664e9203c947e03d0f2b2bb7233853c5583edefa
yyNoBug <yaoyuan2000@sjtu.edu.cn>
2019-10-31 20:47:40 +0800

    Line expression can be displayed now

--- web/javascript/data.js
+++ web/javascript/data.js
@@ -1,78 +1,32 @@
 //window.buildinfo = document.querySelector("div.container-fluid pre").innerHTML;
 window.buildinfo = 
-"L 1 1@\n\
-P 5@\n\
+"#1: N 0 25@\n\
+#2: N 1 17@\n\
+#3: +@\n\
+#4: L 0@\n\
+#5: N 1@\n\
+#6: S 2@\n\
+#7: P 2@\n\
 Step 1\n\
 ~ 0\n\
 1 1\n\
-5 0\n\
+5 25\n\
 -1\n\
 Step 2\n\
 ~ 0\n\
-1 1\n\
-6 1\n\
--1\n\
+2 1\n\
+6 17\n\
+5\n\
 Step 3\n\
-~ 0\n\
-1 1\n\
-7 2\n\
++ 0\n\
+3 0\n\
 -1\n\
 Step 4\n\
-~ 0\n\
-1 1\n\
-8 100\n\
--1\n\
-Step 5\n\
-~ 0\n\
-2 1\n\
-9 1\n\
--1\n\
-Step 6\n\
-- 0\n\
-1 0\n\
--1\n\
-Step 7\n\
-- 0\n\
-2 1\n\
-1 100\n\
-25\n\
-Step 8\n\
-- 0\n\
-1 1\n\
-2 1\n\
--1\n\
-Step 9\n\
-- 0\n\
-1 4\n\
-3 0\n\
-4 99\n\
-1 0\n\
-2 0\n\
--1\n\
-Step 10\n\
-- 0\n\
-2 0\n\
--1\n\
-Step 11\n\
-- 0\n\
-1 1\n\
-8 99\n\
--1\n\
-Step 12\n\
 + 0\n\
-1 1\n\
-4 0\n\
+4 1\n\
+1 25\n\
 -1\n\
-Step 13\n\
-+ 0\n\
-2 1\n\
-1 0\n\
--1\n\
-Step 14\n\
-+ 0\n\
-1 1\n\
-2 1\n\
--1\n";
+Line 4: Invalid operation!";
 
 const store_size = 10;
 const digits = 8;
@@ -128,26 +82,48 @@ function readin() {
         read_length = start + end + 2;
     }
 
+    var runtimeerror = false;
+    if (buildinfo.search("Line") != -1) runtimeerror = true;
+
     while (read_length < input_length) {
         data_set.size++;
 
-        /*var start = read_length;
-        var sub = buildinfo.slicce(start, end);
-        if (sub.search("Line") == -1) {
+        var start = read_length;
+        var sub = buildinfo.slice(start, start + 5);
+        if (sub.search("Line") == 0) {
+            sub = buildinfo.slice(start, buildinfo.length);
             
-        }*/
+            data_set.content[data_set.size] = new Object();
+            var data_cur = data_set.content[data_set.size];
+            data_cur.change = new Object();
+
+            data_cur.op = "~";
+            data_cur.change.op = true;
+            data_cur.runup = "~";
+            data_cur.change.runup = true;
+            data_cur.digits = new Array();
+            data_cur.change.regs = new Array();
+
+            for (var i = 0; i < 5 + store_size; ++i) {
+                data_cur.digits[i] = new Array();
+                data_cur.change.regs[i] = true;
+                for (var j = 0; j < digits; ++j) {
+                    data_cur.digits[i][j] = "*";
+                }
+            }
+            numbers = sub.match(/[+-]?\d+(?:\.\d+)?/g);
+            data_cur.codenum = parseInt(numbers[0]);
+            data_cur.output = sub;
+            return;
+        }
 
         // Get the substring for information of Step(data_set.size).
         start = read_length + 10 + cal_digit(data_set.size);
         sub = buildinfo.slice(start, input_length);
         var end = sub.search("Step");
-        var run_time_error = false;
         if (end == -1) {
-            end = sub.search("Line");
-            if (end == -1) end = input_length;
-            else {
-                run_time_error = true;
-            }
+            if (runtimeerror) end = sub.search("Line");
+            else end = input_length;
         }
         sub = sub.slice(0, end);
         read_length = start + end;
